6abc4eadf61a98c30937297b2af92bcb8e838377
Alex Charlton <alex.n.charlton@gmail.com>
2024-10-31 20:35:34 -0700

    Functioning Beagle Fire dts

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -5,4 +5,4 @@ set -e
 
 PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/..
 
-dd if=$PROJECT_DIR/build/zephyr.img of=/dev/sdd1  bs=4M status=progress oflag=sync
+dd if=$PROJECT_DIR/build/zephyr.img of=/dev/sdd1 bs=4M status=progress oflag=sync

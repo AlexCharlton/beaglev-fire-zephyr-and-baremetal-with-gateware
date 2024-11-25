ed5d9b369515502b035ec4e822ed96b0c925b29b
Alex Charlton <alex.n.charlton@gmail.com>
2024-11-24 17:03:19 -0800

    Kinda working baremetal rust

--- baremetal/hello-rust/src/lib.rs
+++ baremetal/hello-rust/src/lib.rs
@@ -13,38 +13,38 @@ pub extern "C" fn u54_1() {
     // We'll implement this function to match your C code
     unsafe {
         // Basic hart setup
-        clear_soft_interrupt();
-        set_csr(0x304, 0x8); // mie, MIP_MSIP
+        clear_soft_interrupt_rs();
+        //set_csr_rs(0x304, 0x8); // mie, MIP_MSIP
 
         // Initialize PLIC and enable interrupts
-        PLIC_init();
-        enable_irq();
+        PLIC_init_rs();
+        enable_irq_rs();
 
         // Configure clocks for UART0
-        mss_config_clk_rst(0, 0, 1); // MSS_PERIPH_MMUART0, MPFS_HAL_FIRST_HART, PERIPHERAL_ON
+        mss_config_clk_rst(0, 1, 0); // MSS_PERIPH_MMUART0, MPFS_HAL_FIRST_HART, PERIPHERAL_ON
 
         // Initialize UART0
-        uart_init();
-        uart_puts("\r\n".as_ptr());
-        uart_puts("Hello World from rust!\r\n".as_ptr());
+        uart_init_rs();
+        uart_puts_rs("\r\n".as_ptr());
+        uart_puts_rs("Hello World from rust!\r\n".as_ptr());
     }
 }
 
 // External C functions we need to call
 extern "C" {
-    fn clear_soft_interrupt();
-    fn PLIC_init();
-    fn enable_irq();
+    fn clear_soft_interrupt_rs();
+    fn PLIC_init_rs();
+    fn enable_irq_rs();
     fn mss_config_clk_rst(periph: u32, hart: u8, enable: u32);
-    fn uart_init();
-    fn uart_puts(s: *const u8);
+    fn uart_init_rs();
+    fn uart_puts_rs(s: *const u8);
 }
 
 #[inline]
-unsafe fn set_csr(reg: u32, bit: u32) {
+unsafe fn set_csr_rs(reg: u32, bit: u32) {
     match reg {
         0x304 => core::arch::asm!("csrs mie, {}", in(reg) bit),
         // Add other CSR cases as needed
-        _ => panic!("Unsupported CSR register"),
+        x => panic!("Unsupported CSR register: {}", x),
     }
 }
---
+++ scripts/init-baremetal-wsl.gdb
@@ -0,0 +1,4 @@
+target extended-remote host.docker.internal:3333
+thread 2
+set $pc = 0x80000000
+break u54_1

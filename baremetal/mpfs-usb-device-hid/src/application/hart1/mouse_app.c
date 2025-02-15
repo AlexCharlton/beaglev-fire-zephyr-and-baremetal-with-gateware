ebf0dbd387309b43ce370f6a2483f82281d52b64
Alex Charlton <alex.n.charlton@gmail.com>
2025-02-15 12:59:48 -0800

    Fix usb-device-hid example from cold boot

--- baremetal/mpfs-usb-device-hid/src/application/hart1/mouse_app.c
+++ baremetal/mpfs-usb-device-hid/src/application/hart1/mouse_app.c
@@ -1,15 +1,15 @@
-/***************************************************************************//**
- * Copyright 2023 Microchip FPGA Embedded Systems Solutions.
- *
- * SPDX-License-Identifier: MIT
- *
- * USB HID Class Mouse Device example application to demonstrate the PolarFire
- * SoC MSS USB operations in device mode.
- *
- * This file uses MSS USB Driver stack (inclusive of USBD-HID class driver) to
- * connect to USB Host as USB HID Mouse device.
- *
- */
+/***************************************************************************/ /**
+                                                                               * Copyright 2023 Microchip FPGA Embedded Systems Solutions.
+                                                                               *
+                                                                               * SPDX-License-Identifier: MIT
+                                                                               *
+                                                                               * USB HID Class Mouse Device example application to demonstrate the PolarFire
+                                                                               * SoC MSS USB operations in device mode.
+                                                                               *
+                                                                               * This file uses MSS USB Driver stack (inclusive of USBD-HID class driver) to
+                                                                               * connect to USB Host as USB HID Mouse device.
+                                                                               *
+                                                                               */
 
 #include "mouse_app.h"
 #include "drivers/mss/mss_gpio/mss_gpio.h"
@@ -17,66 +17,63 @@
 #include "drivers/mss/mss_usb/mss_usb_device_hid.h"
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#define DELAY_BETWEEN_SENSES                            50000u
+#define DELAY_BETWEEN_SENSES 500000u
 
-/*This buffer is passed to the USB driver. When USB drivers are configured to
-use internal DMA, the address of this buffer must be modulo-4.Otherwise DMA
-Transfer will fail.*/
-mss_usbd_hid_report_t report __attribute__ ((aligned (4))) = {0};
+    /*This buffer is passed to the USB driver. When USB drivers are configured to
+    use internal DMA, the address of this buffer must be modulo-4.Otherwise DMA
+    Transfer will fail.*/
+    mss_usbd_hid_report_t report __attribute__((aligned(4))) = {0};
 
-/* USB descriptors for HID class mouse enumeration. */
-extern mss_usbd_user_descr_cb_t hid_mouse_descriptors_cb;
+    /* USB descriptors for HID class mouse enumeration. */
+    extern mss_usbd_user_descr_cb_t hid_mouse_descriptors_cb;
 
-void
-MOUSE_task
-(
     void
-)
-{
-    volatile uint32_t delay;
-
-    if(1 == MSS_USBD_HID_tx_done())
+    MOUSE_task(
+        void)
     {
-        MSS_USBD_HID_tx_report ((uint8_t*)&report, sizeof(report));
-    }
-    else
-    {
-        for(delay = 0; delay < DELAY_BETWEEN_SENSES; ++delay)
-        {
-            ;
-        }
+        volatile uint32_t delay;
 
-        if(report.x_move < (int8_t)30)
+        if (1 == MSS_USBD_HID_tx_done())
         {
-            ++report.x_move;
+            MSS_USBD_HID_tx_report((uint8_t *)&report, sizeof(report));
         }
         else
         {
-            report.x_move = -30;
+            for (delay = 0; delay < DELAY_BETWEEN_SENSES; ++delay)
+            {
+                ;
+            }
+
+            if (report.x_move < (int8_t)30)
+            {
+                ++report.x_move;
+            }
+            else
+            {
+                report.x_move = -30;
+            }
         }
     }
-}
 
-void
-MOUSE_init
-(
-void
-)
-{
-    /* Assign call-back function handler structure needed by USB Device Core
-     * driver
-     */
-    MSS_USBD_set_descr_cb_handler(&hid_mouse_descriptors_cb);
+    void
+    MOUSE_init(
+        void)
+    {
+        /* Assign call-back function handler structure needed by USB Device Core
+         * driver
+         */
+        MSS_USBD_set_descr_cb_handler(&hid_mouse_descriptors_cb);
 
-    /* Initialize HID Class driver. */
-    MSS_USBD_HID_init(MSS_USB_DEVICE_HS);
+        /* Initialize HID Class driver. */
+        MSS_USBD_HID_init(MSS_USB_DEVICE_HS);
 
-    /* Initialize USB Device Core driver. */
-    MSS_USBD_init(MSS_USB_DEVICE_HS);
-}
+        /* Initialize USB Device Core driver. */
+        MSS_USBD_init(MSS_USB_DEVICE_HS);
+    }
 
 #ifdef __cplusplus
 }
--- baremetal/mpfs-usb-device-hid/src/application/hart1/u54_1.c
+++ baremetal/mpfs-usb-device-hid/src/application/hart1/u54_1.c
@@ -22,13 +22,6 @@
  * Instruction message. This message will be transmitted over the UART when
  * the program starts.
  *****************************************************************************/
-
-const uint8_t g_message1[] =
-    "\r\nMSS MPU configuration for USB failed\r\n";
-
-const uint8_t g_message2[] =
-    "\r\nMSS MPU configured for USB\r\n";
-
 const uint8_t g_message4[] =
     "\r\nThis feature automatically moves the mouse pointer horizontally"
     " (x-direction) on the desktop to which this USB port is connected.\r\n";
@@ -38,59 +31,30 @@ const uint8_t g_message4[] =
  */
 void u54_1(void)
 {
-    uint8_t mss_mpu_cfg_status = 0u;
-
     /* Clear pending software interrupt in case there was any.
      * Enable only the software interrupt so that the E51 core can bring this
      * core out of WFI by raising a software interrupt. */
     clear_soft_interrupt();
     set_csr(mie, MIP_MSIP);
 
-#if (IMAGE_LOADED_BY_BOOTLOADER == 0)
-
-    /* Put this hart in WFI. */
-    do
-    {
-        __asm("wfi");
-    } while (0 == (read_csr(mip) & MIP_MSIP));
-
-    /* The hart is now out of WFI, clear the SW interrupt. Here onwards the
-     * application can enable and use any interrupts as required */
-    clear_soft_interrupt();
-
-#endif
-
     /* Bring the MMUART0 and USB out of Reset */
     (void)mss_config_clk_rst(MSS_PERIPH_MMUART0, (uint8_t)1, PERIPHERAL_ON);
-    (void)mss_config_clk_rst(MSS_PERIPH_USB, (uint8_t)1, PERIPHERAL_ON);
 
     /* All clocks ON */
     MSS_UART_init(&g_mss_uart0_lo,
                   MSS_UART_115200_BAUD,
                   MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
 
-    mss_mpu_cfg_status = MSS_MPU_configure(MSS_MPU_USB, MSS_MPU_PMP_REGION1,
-                                           0x08000000u, 0x200000,
-                                           MPU_MODE_READ_ACCESS | MPU_MODE_WRITE_ACCESS | MPU_MODE_EXEC_ACCESS,
-                                           MSS_MPU_AM_NAPOT, 0);
-
-    if (mss_mpu_cfg_status)
-    {
-        /* Message on uart0 */
-        MSS_UART_polled_tx(&g_mss_uart0_lo, g_message1, sizeof(g_message1));
-    }
-    else
-
-    {
-        /* Message on uart0 */
-        MSS_UART_polled_tx(&g_mss_uart0_lo, g_message2, sizeof(g_message2));
-    }
+    // Disable and enable USB peripheral
+    // NOTE: It's is super important to disable first (unlike most peripherals)
+    //       Otherwise, DMA won't work
+    (void)mss_config_clk_rst(MSS_PERIPH_USB, (uint8_t)1, PERIPHERAL_OFF);
+    (void)mss_config_clk_rst(MSS_PERIPH_USB, (uint8_t)1, PERIPHERAL_ON);
 
     PLIC_init();
 
     PLIC_SetPriority(USB_DMA_PLIC, 2);
     PLIC_SetPriority(USB_MC_PLIC, 2);
-
     PLIC_EnableIRQ(USB_DMA_PLIC);
     PLIC_EnableIRQ(USB_MC_PLIC);
 
